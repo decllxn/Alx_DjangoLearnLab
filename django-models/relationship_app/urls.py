0bd2587e0a51f4f832a5a37055221b8b1aabbc3b
Declan Munene <munene.declan1@gmail.com>
2024-08-18 20:59:00 +0300

    Django Authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,5 +4,8 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
 ]
 
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
 def list_books(request):
     books = Book.objects.all()
@@ -11,3 +15,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your home page URL name
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
