2861c5298fb480fefdf1f2f312f96cfe216ac33a
Declan Munene <munene.declan1@gmail.com>
2024-09-01 22:37:39 +0300

    Auth and perms

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
@@ -6,5 +7,6 @@ router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='books')
 
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
