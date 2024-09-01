2c6c77dd087a1e1760d686aacdbbf5ce8eb36445
Declan Munene <munene.declan1@gmail.com>
2024-09-01 22:27:51 +0300

    Implementing CRUD operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from .views import BookListCreateAPIView
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='books')
 
 urlpatterns = [
-    path('books/', views.BookListCreateAPIView.as_view(), name="book_list_create")
+    path('api/', include(router.urls))
 ]
