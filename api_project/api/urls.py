67f32455b059df69dcbac45358736530d305c28a
Declan Munene <munene.declan1@gmail.com>
2024-09-01 22:11:34 +0300

    Created my api end point

---
+++ api_project/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import BookListCreateAPIView
+from . import views
+
+urlpatterns = [
+    path('books/', views.BookListCreateAPIView.as_view(), name="book_list_create")
+]
