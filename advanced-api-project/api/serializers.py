7fd5b4f810d66e4bed8ddede61af6cf994688e8c
Declan Munene <munene.declan1@gmail.com>
2024-09-08 14:43:39 +0300

    Author Serializers implementations

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from .models import Author, Book
 from datetime import datetime
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
 class BookSerializer(serializers.ModelSerializer):
+    authors = AuthorSerializer(many=True, read_only=True)
     class Meta:
         model = Book
         fields = '__all__'
@@ -11,11 +17,9 @@ class BookSerializer(serializers.ModelSerializer):
         """
         Ensure that the publication_year is not in the future.
         """
+        
         current_year = datetime.now().year
         if value > current_year:
             raise serializers.ValidationError('Publication year cannot be in the future')
         return value
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
+    
