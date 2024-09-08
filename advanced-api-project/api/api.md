80b3c05863aac27d2c26376e8df7e025960bfd4a
Declan Munene <munene.declan1@gmail.com>
2024-09-08 15:07:19 +0300

    Api Project

---
+++ advanced-api-project/api/api.md
@@ -0,0 +1,13 @@
+### API Query Parameters
+
+- **Filtering:**
+  - `?title=<title>`: Filter by book title.
+  - `?author=<author_name>`: Filter by author name.
+  - `?publication_year=<year>`: Filter by publication year.
+
+- **Searching:**
+  - `?search=<query>`: Search books by title or author name.
+
+- **Ordering:**
+  - `?ordering=title`: Order books by title (ascending).
+  - `?ordering=-publication_year`: Order books by publication year (descending)
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author_name']
+    ordering_fields = ['title', 'publication_year']
