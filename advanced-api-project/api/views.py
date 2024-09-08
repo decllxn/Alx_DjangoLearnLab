11c78999f280e1103c0c2dce6775230ff0392253
Declan Munene <munene.declan1@gmail.com>
2024-09-08 15:39:25 +0300

    Adding imports

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework import generics
 from django_filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from django_filters.rest_framework import DjangoFilterBackend
