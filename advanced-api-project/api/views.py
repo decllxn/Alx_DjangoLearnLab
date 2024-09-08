2edc38019448522755dc47be8c80072757117f8e
Declan Munene <munene.declan1@gmail.com>
2024-09-08 15:42:22 +0300

    Added list Api view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -8,6 +8,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    ordering_fields = ['username', 'email']
+
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
