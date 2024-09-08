c3afe1bf67983412d5b28606d02e2d30ddbd14f9
Declan Munene <munene.declan1@gmail.com>
2024-09-08 14:33:59 +0300

    Adding Serializers

---
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Author, Book
+from datetime import datetime
+
+class BookSerializer(serializers.ModelSerializers):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        """
+        Ensure that the publication_year is not in the future.
+        """
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError('Publication year cannot be in the future')
+        return value
+class AuthorSerializer(serializers.ModelSerializers):
+    class Meta:
+        model = Author
+        fields = '__all__'
