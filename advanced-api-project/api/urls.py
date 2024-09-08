9bf14bd34db65a7f7fba26f632eb86720f350444
Declan Munene <munene.declan1@gmail.com>
2024-09-08 15:20:53 +0300

    Url Fixes

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),  # URL for listing all books
     path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),  # URL for retrieving a specific book
     path('books/create/', CreateView.as_view(), name='book-create'),  # URL for creating a new book
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),  # URL for updating a specific book
-    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),  # URL for deleting a specific book
+    path('books/update/', UpdateView.as_view(), name='book-update'),  # URL for updating a specific book
+    path('books/delete/', DeleteView.as_view(), name='book-delete'),  # URL for deleting a specific book
 ]
