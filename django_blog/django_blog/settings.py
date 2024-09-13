8934774d4b6ca20a33e4e1768066c366e436814d
Declan Munene <munene.declan1@gmail.com>
2024-09-14 01:28:32 +0300

    Configured Postgresql db

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'decllxn',
+        'PASSWORD': 'King Declan1234',
+        'HOST': 'localhost',  # Or your PostgreSQL server address
+        'PORT': '5432',       # PostgreSQL default port
     }
 }
 
