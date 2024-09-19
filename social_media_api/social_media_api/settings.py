8c65cf9c1f783df96bcbf9a503a62363dcee8abd
Declan Munene <munene.declan1@gmail.com>
2024-09-19 20:24:23 +0300

    Created apis

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -94,8 +95,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',  # Optional if you want session auth
+        'rest_framework.authentication.SessionAuthentication',  
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
