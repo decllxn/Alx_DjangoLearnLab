d8688d139b13ff9be659ff2498128c51c7474f36
Declan Munene <munene.declan1@gmail.com>
2024-09-19 19:39:58 +0300

    Hot fix

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import CustomUser
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
+CustomUser = get_user_model()
 
-class CustomUserSerialzier(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
@@ -11,17 +13,21 @@ class CustomUserSerialzier(serializers.ModelSerializer):
     
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Create a new user using the create_user method from Django's User model
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+        # Generate an authentication token for the newly created user
+        Token.objects.create(user=user)
         return user
 
 
